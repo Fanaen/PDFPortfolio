c95cbdf7ce3282a7ab603e4bcfd4b846634a8cc4
Fanaen <contact@fanaen.fr>
2015-11-28 18:06:00 +0100

    Add Less compiling

--- showreel.js
+++ showreel.js
@@ -1,23 +1,15 @@
 var wkhtmltopdf = require('wkhtmltopdf'),
     jade        = require('jade'),
+    less        = require('less'),
     fs          = require('fs'),
     config      = require('./config/config.js');
 
 // Create Jade's configuration --
-var options = {
+var jadeOptions = {
   youAreUsingJade: true,
+  css: '',
   absolutePath: 'file:///' + __dirname.replace(/\\/g, '/') // Fix content not found error --
 };
-
-// Use the template --
-var html = jade.renderFile('template/index.jade', options);
-console.log("Html rended from index.jade.");
-
-// Print the HTML --
-fs.writeFile(config.output + '.html', html, function(err) {
-  if(err) { return console.log(err); }
-  console.log('File created: '+ config.output + '.html');
-});
 
 // Create wkhtmltopdf's configuration --
 var wkOptions = {
@@ -25,13 +17,67 @@ var wkOptions = {
   enableLocalFileAccess : true
 };
 
-// Render the PDF --
-wkhtmltopdf(html, wkOptions, function (code, signal) {
-  if(code) {
-    console.error(code);
-    console.error("Please check if the file is writable.");
-    return;
-  }
+// Render the css --
+var renderCSS = function() {
+  // Read the style file --
+  fs.readFile('template/style.less', {encoding: 'UTF-8'}, function(err,data){
+      if (!err){
 
-  console.log('File created: '+ config.output + '.pdf');
-});
+        // Compute the file --
+        less.render(data, {
+            paths: ['.', './node_modules/bootstrap/less'],  // Specify search paths for @import directives
+            filename: 'style.less', // Specify a filename, for better error messages
+            compress: true          // Minify CSS output
+          },
+          function (e, output) {
+            jadeOptions.css = output.css;
+            console.log("CSS rended from style.less.");
+
+            // Print the HTML --
+            fs.writeFile(config.output + '.css', output.css, function(err) {
+              if(err) { return console.log(err); }
+              console.log('File created: '+ config.output + '.css');
+            });
+
+            // Pursue the rendering --
+            renderHTML();
+        });
+      }
+      else{
+        console.log(err);
+      }
+  });
+};
+
+// Render the HTML --
+var renderHTML = function() {
+  // Use the template --
+  var html = jade.renderFile('template/index.jade', jadeOptions);
+  console.log("Html rended from index.jade.");
+
+  // Print the HTML --
+  fs.writeFile(config.output + '.html', html, function(err) {
+    if(err) { return console.log(err); }
+    console.log('File created: '+ config.output + '.html');
+  });
+
+  // Pursue the rendering --
+  renderPDF(html);
+};
+
+// Render the PDF --
+var renderPDF = function(html) {
+  // Render the PDF --
+  wkhtmltopdf(html, wkOptions, function (code, signal) {
+    if(code) {
+      console.error(code);
+      console.error("Please check if the file is writable.");
+      return;
+    }
+
+    console.log('File created: '+ config.output + '.pdf');
+  });
+};
+
+// Run the rendering --
+renderCSS();
