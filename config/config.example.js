a114b481abfa7a06917ce53a9bdf237d3e31f3bf
Fanaen <contact@fanaen.fr>
2015-11-28 15:53:15 +0100

    Add a config system

---
+++ config/config.example.js
@@ -0,0 +1,5 @@
+var config = {};
+
+config.output = 'out/Showreel.pdf';
+
+module.exports = config;
--- showreel.js
+++ showreel.js
@@ -1,3 +1,4 @@
-var wkhtmltopdf = require('wkhtmltopdf');
+var wkhtmltopdf = require('wkhtmltopdf'),
+    config      = require('./config/config.js');
 
-wkhtmltopdf('http://google.com/', { output: 'out/Showreel.pdf' });
+wkhtmltopdf('http://google.com/', { output: config.output });
